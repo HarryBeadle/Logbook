496474b1d9a5408a3b53f1380c34e62a73c6a88f
Harry Beadle <harry.beadle@gmail.com>
2017-01-11 11:35:00 +0000

    Now using argparse

--- log.py
+++ log.py
@@ -4,51 +4,33 @@
 from os import popen
 from sys import argv
 
-help = """Logbook by Harry Beadle
-harrybeadle.xyz
+import argparse
 
-Outputs (appends) to logbook.html in working directory.
+# Parse Arguments
+parser = argparse.ArgumentParser(description = "Appends a log to logbook.html")
+parser.add_argument('text', metavar='s', type=str, 
+	help = "Text for log, title or section.")
+parser.add_argument('-I', action="store_true", help="Important")
+parser.add_argument('-t', action="store_true", help="Title")
+parser.add_argument('-s', action="store_true", help="Section")
+parser.add_argument('-c', default='black', type=str, help="Color the text this CSS color")
 
-Usage: log [option] [argument] text
-
-Options:
-	-t         Title
-	-s         Section
-	-I         Important
-	-c [color] Color the text this CSS color.
-"""
-
-if argv[1] in ["help", "h", "-h", "-help"]:
-	print help
-	quit()
+args = parser.parse_args()
+print "Text", args.text
 
 with open("logbook.html", 'a') as logbook:
 	logbook.write("<small>" + popen("echo $USER").read()[:-1] + " commited at " + popen("date", 'r').read()[:-1] + "</small><br />")
 	print "//", popen("date", 'r').read()[:-1]
-	if argv[1] == "-t":
+	if args.I:
+		logbook.write("<b style='color:red;'>Important</b><br />")
+	logbook.write("<span style='color:" + args.c + ";'>")
+	if args.t:
 		# Title
-		logbook.write("<h1> " + " ".join(argv[2:]) + " </h1>")
-		print "Title:", " ".join(argv[2:])
-	elif argv[1] == "-s":
+		logbook.write("<h1> " + args.text + " </h1>")
+	elif args.s:
 		# Header
-		logbook.write("<h2> " + " ".join(argv[2:]) + " </h2>")
-		print "Section:", " ".join(argv[2:])
-	elif argv[1] == "-I":
-		logbook.write("<span style='color:red;'><b>Important</b><br>")
-		logbook.write(" ".join(argv[2:]))
-		logbook.write("</span>")
-		logbook.write("\n<hr>")
-		print "Important"
-		print " ".join(argv[2:])
-	elif argv[1] == "-c":
-		# Color
-		logbook.write("<span style='color:" + argv[2] + ";'>")
-		logbook.write(" ".join(argv[3:]))
-		logbook.write("</span>")
-		logbook.write("\n<hr>")
-		print " ".join(argv[3:])
+		logbook.write("<h2> " + args.text + " </h2>")
 	else:
 		# Entry
-		logbook.write(" ".join(argv[1:]))
-		logbook.write("\n<hr>")
-		print " ".join(argv[1:])
+		logbook.write(args.text)
+	logbook.write("\n<hr></span>")
