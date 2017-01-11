495277fdb6bc4e4be6e845b8f8a987bba93b1eac
Harry Beadle <harry.beadle@gmail.com>
2017-01-11 17:47:00 +0000

    Multiple arguments called at once works

--- log.py
+++ log.py
@@ -11,14 +11,13 @@ import datetime
 parser = argparse.ArgumentParser(
     description = "Appends a log to logbook.html")
 
-parser.add_argument('text', metavar='MESSAGE', type=str,
-    help = "Text for log, title or section.")
-parser.add_argument('-I', action="store_true", help="Important")
-parser.add_argument('-t', action="store_true", help="Title")
-parser.add_argument('-s', action="store_true", help="Section")
-parser.add_argument('-c', default='black', type=str,
+parser.add_argument('-m', metavar='message', type=str, help = "Text for log, title or section.")
+parser.add_argument('-I', action='store_true', help="Important")
+parser.add_argument('-t', metavar="title", type=str, help="Title")
+parser.add_argument('-s', metavar="section header", type=str, help="Section")
+parser.add_argument('-c', metavar='color', default='black', type=str,
     help="Color the text this CSS color")
-parser.add_argument('-f', type=str, nargs='*',
+parser.add_argument('-f', metavar='path', type=str, nargs='*',
     help="File to be attached to log.")
 args = parser.parse_args()
 
@@ -31,13 +30,13 @@ with open("logbook.html", 'a') as logbook:
     logbook.write("<span style='font-family:monospace; color:" + args.c + ";'>")
     if args.t:
         # Title
-        logbook.write("<h1 style='font-family:serif;'> " + args.text + " </h1>")
-    elif args.s:
+        logbook.write("<h1 style='font-family:serif;'>" + args.t + "</h1>")
+    if args.s:
         # Header
-        logbook.write("<h2 style='font-family:serif;'> " + args.text + " </h2>")
-    else:
+        logbook.write("<h2 style='font-family:serif;'>" + args.s + "</h2>")
+    if args.m:
         # Entry
-        logbook.write(args.text)
+        logbook.write(args.m)
     if args.f:
         logbook.write("<p>")
         if not os.path.exists("files"):
@@ -47,6 +46,6 @@ with open("logbook.html", 'a') as logbook:
             name = os.path.basename(files)
             dst = os.path.join("files", datemark + '-' + name)
             copyfile(files, dst)
-            logbook.write("<a style='text-decoration:none;border:solid 1px;padding:0.25em 0.5em 0.25em 0.5em;' href='" + dst + "'>💾 " + name + "</a> ")
+            logbook.write("<a style='text-decoration:none;border:solid 1px;padding:0.25em 0.5em 0.25em 0.5em;' href='" + dst + "'>💾 " + name + "</a> ") # Space at end required!
         logbook.write("</p>")
     logbook.write("\n<hr></span>")
