56fa2fb47e13a8c25b7cc5ce454a098d2d7397f1
Harry Beadle <harry.beadle@gmail.com>
2017-01-20 21:35:58 +0000

    New install script written in python

---
+++ install.py
@@ -0,0 +1,25 @@
+# Install Logbook
+# Harry Beadle and Rhys Thomas
+
+# harrybeadle.xyz
+
+import platform, os
+from shutil import copyfile
+
+p = platform.system()
+
+if p == "Linux":
+	if os.path.isdir("~/.bin"):
+    	dir = "~/.bin/"
+	else:
+	    dir = "/usr/local/bin"
+
+	print("Copying files to %s." % dir)
+	copyfile("log.py", dir + "/log")
+	print("Done.")
+
+
+if p == "Windows":
+	print("Windows is not currently supported by the installer.")
+	print("Please install manually.")
+
