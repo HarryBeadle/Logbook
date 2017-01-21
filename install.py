ebac7053d2650d734b993e166ad87c1c786a2944
Rhys Thomas <rt8g15@soton.ac.uk>
2017-01-21 10:40:56 +0000

    Replaced tabs with spaces.

--- install.py
+++ install.py
@@ -9,17 +9,17 @@ from shutil import copyfile
 p = platform.system()
 
 if p == "Linux":
-	if os.path.isdir("~/.bin"):
-    	dir = "~/.bin/"
-	else:
-	    dir = "/usr/local/bin"
+    if os.path.isdir("~/.bin"):
+        dir = "~/.bin/"
+    else:
+        dir = "/usr/local/bin"
 
-	print("Copying files to %s." % dir)
-	copyfile("log.py", dir + "/log")
-	print("Done.")
+    print("Copying files to %s." % dir)
+    copyfile("log.py", dir + "/log")
+    print("Done.")
 
 
 if p == "Windows":
-	print("Windows is not currently supported by the installer.")
-	print("Please install manually.")
+    print("Windows is not currently supported by the installer.")
+    print("Please install manually.")
 
